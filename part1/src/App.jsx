b17c2577a789dca93b7fd9b3391d5915cb488614
Wolf Pinz <wolfpinz@gmail.com>
2024-03-04 15:43:26 +0100

    finish ex 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,32 +1,33 @@
 const Header = (props) => {
-  console.log(props)
   return (
     <h1>{props.course}</h1>
   )
 }
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
       <p>
-      {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
+        {props.part} {props.exercises}
       </p>
     </>
   )
 }
-
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  )
+}
 const Total = (props) => {
   return (
     <p>Number of exercises {props.total}</p>
   )
 }
-
-const App = () => {
+export const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -51,4 +52,4 @@ const App = () => {
   )
 }
 
-export default App
+// export default App
