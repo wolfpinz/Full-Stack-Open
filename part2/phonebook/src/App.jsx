9af0998e7c795fecd441efe03cc4f52f79f718c8
Wolf Pinz <wolfpinz@gmail.com>
2024-03-06 13:46:13 +0100

    finish phonebook step4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phoneNumber: '1234567789' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const personsToShow = showAll
+    ? persons
+    : persons.filter((person) => person.name.includes(newFilter))
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -27,6 +36,10 @@ const App = () => {
   const handlePhoneNumberInputChange = (event) => {
     setNewPhoneNumber(event.target.value)
   }
+  const handleFilterInputChange = (event) => {
+    setNewFilter(event.target.value)
+    setShowAll(false)
+  }
 
   const doesNameAlreadyExist = () => {
     const namesArray = persons.map((p) => p.name)
@@ -36,6 +49,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown: <input value={newFilter} onChange={handleFilterInputChange} /></div>
+      <h2>Add new</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleNameInputChange} /></div>
         <div>phone: <input value={newPhoneNumber} onChange={handlePhoneNumberInputChange} /></div>
@@ -44,7 +59,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map((person) => {
+        {personsToShow.map((person) => {
           return <div key={person.name}>{person.name} {person.phoneNumber && `(📞: ${person.phoneNumber})`}</div>
         })}
     </div>
